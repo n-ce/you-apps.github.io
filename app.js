1c1ae500b33609781463bccb0751bf2ccbf3d666
Animesh <69345507+n-ce@users.noreply.github.com>
2025-11-04 23:46:38 +0530

    Param destructuring

--- app.js
+++ app.js
@@ -1,39 +1,32 @@
 const fetchData = path => fetch(`https://api.github.com/orgs/you-apps/${path}`).then(res => res.json());
 
-Promise
-    .all([fetchData('repos'), fetchData('members')])
+Promise.all([fetchData('repos'), fetchData('members')])
     .then(([repos, members]) => {
-
         document.querySelector("#apps > div").replaceChildren(...repos
             .filter(repo => repo.name.endsWith("You") && !repo.archived)
             .sort((a, b) => b.stargazers_count - a.stargazers_count)
-            .map(repo => {
+            .map(({ name, html_url, description, stargazers_count }) => {
                 const card = document.createElement('a');
                 card.className = 'card';
-                card.href = repo.html_url;
+                card.href = html_url;
 
                 const img = document.createElement('img');
-                img.alt = `${repo.name} icon`;
-                img.src = `https://raw.githubusercontent.com/you-apps/${repo.name}/main/fastlane/metadata/android/en-US/images/icon.png`;
+                img.alt = `${name} icon`;
+                img.src = `https://raw.githubusercontent.com/you-apps/${name}/main/fastlane/metadata/android/en-US/images/icon.png`;
                 
                 const div = document.createElement('div');
-                
                 const h3 = document.createElement('h3');
-                h3.textContent = repo.name;
-                
+                h3.textContent = name;
                 const p = document.createElement('p');
-                p.textContent = repo.description;
-
+                p.textContent = description;
                 div.append(h3, p);
 
                 const span = document.createElement('span');
                 span.className = 'stars';
-                
                 const starImg = document.createElement('img');
                 starImg.src = 'assets/star.svg';
                 starImg.alt = 'Star icon';
-                
-                span.append(starImg, document.createTextNode(` ${repo.stargazers_count}`));
+                span.append(starImg, document.createTextNode(` ${stargazers_count}`));
 
                 card.append(img, div, span);
                 return card;
@@ -41,17 +34,17 @@ Promise
         );
         
         document.querySelector("#team > div").replaceChildren(...members
-            .map(member => {
+            .map(({ login, html_url, avatar_url }) => {
                 const card = document.createElement('a');
                 card.className = 'card';
-                card.href = member.html_url;
+                card.href = html_url;
 
                 const img = document.createElement('img');
-                img.src = member.avatar_url;
-                img.alt = `${member.login}'s avatar`;
+                img.src = avatar_url;
+                img.alt = `${login}'s avatar`;
 
                 const h3 = document.createElement('h3');
-                h3.textContent = member.login;
+                h3.textContent = login;
                 
                 card.append(img, h3);
                 return card;
